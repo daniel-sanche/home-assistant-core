9a27c4bcfd487bc1f47673b79aa6c2bbb60514e7
Daniel Sanche <d.sanche14@gmail.com>
2024-06-19 10:35:02 -0700

    added code for getting past history

--- homeassistant/components/sanche/__init__.py
+++ homeassistant/components/sanche/__init__.py
@@ -2,11 +2,13 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from datetime import datetime, timedelta
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.event import TrackStates, async_track_state_change_event
+from homeassistant.components.recorder import history
 
 from .const import DOMAIN
 
@@ -68,6 +70,12 @@ event_listener = None
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up sanche-test from a config entry."""
     print("start setup entry")
+    # history_dict = {}
+    # for entity_id in entry.data["entities"]:
+    #     print(entity_id)
+    #     # https://github.com/home-assistant/core/blob/6a3778c48eb0db8cbc59406f27367646e4dbc7f3/homeassistant/components/recorder/history/__init__.py#L155
+    #     entity_history = await hass.async_add_executor_job(history.state_changes_during_period, hass, (datetime.now()- timedelta(days=1)).astimezone(), datetime.now().astimezone(), entity_id, True)
+    #     history_dict.update(entity_history)
 
     hass.data.setdefault(DOMAIN, {})
     # TODO 1. Create API instance
