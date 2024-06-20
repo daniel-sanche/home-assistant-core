1e89efb4b627b499cc81a6ae3721128ea6916ed8
Daniel Sanche <d.sanche14@gmail.com>
2024-06-19 19:31:14 -0700

    save data to db

--- homeassistant/components/sanche/server.py
+++ homeassistant/components/sanche/server.py
@@ -4,7 +4,11 @@ import pymongo
 app = Flask(__name__)
 
 mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = mongo_client["events"]
+db = mongo_client["lifeline"]
+timeline_collection = db["timeline"]
+# add indices
+timeline_collection.create_index("start_time")
+timeline_collection.create_index("entity_id")
 
 @app.route('/')
 def hello_world():
@@ -14,7 +18,21 @@ def hello_world():
 def new_data():
     data = request.get_json()
     entity_id = data['entity_id']
-    x = db[entity_id].insert_one(data)
+    # find most recent entry for entity_id
+    most_recent = timeline_collection.find_one(sort=[("start_time", pymongo.DESCENDING)])
+    if most_recent is not None:
+        if data["prev_uid"] != most_recent["_id"]:
+            return "ERROR: prev_uid must match most recent entry", 400
+        if data["start_time"] <= most_recent["start_time"]:
+            return "ERROR: start_time must be greater than most recent entry", 400
+    formatted_data = {
+        "_id": data["uid"],
+        "entity_id": data["entity_id"],
+        "start_time": data["start_time"],
+        "value": data["value"]
+    }
+
+    x = timeline_collection.insert_one(formatted_data)
 
     # print the size of the collection
     print(db[entity_id].count_documents({}))
