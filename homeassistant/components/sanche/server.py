d056e5151c5a9ca49b3c0717d3b5f965ae0900d7
Daniel Sanche <d.sanche14@gmail.com>
2024-06-19 17:36:26 -0700

    added mongodb

--- homeassistant/components/sanche/server.py
+++ homeassistant/components/sanche/server.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
+import pymongo
 
 app = Flask(__name__)
+
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = mongo_client["events"]
 
 @app.route('/')
 def hello_world():
@@ -9,9 +13,26 @@ def hello_world():
 @app.route('/new_data', methods=['POST'])
 def new_data():
     data = request.get_json()
-    print(data)
-    return data
+    entity_id = data['entity_id']
+    x = db[entity_id].insert_one(data)
+
+    # print the size of the collection
+    print(db[entity_id].count_documents({}))
+
+    return "OK"
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    # print out database
+    for collection in db.list_collection_names():
+        print(collection)
+        for x in db[collection].find():
+            print(x)
+    # return size of database
+    return str(db.list_collection_names())
 
 if __name__ == '__main__':
     port = 7070
     app.run(host='0.0.0.0', port=port)
+    # start mongo with:
+    # docker run -p 27017:27017 -it mongo:latest
